c146457e93d8809fb0dc9f5d7b5728f7e96fe856
Rahul Kumar <irahulkv@gmail.com>
2022-08-19 23:49:37 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>Trending</div>
-  )
-}
+  return <div>Trending</div>;
+};
 
-export default App
+export default App;
